c9f9757e458f7c2d206c0c6a35c32f1b7af62483
Martin LE-ROY <mle-roy@e2r3p8.42.fr>
2014-04-20 18:14:41 +0200

    bouffon

--- ft_malloc.c
+++ ft_malloc.c
@@ -1,38 +1,123 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_malloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mle-roy <mle-roy@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/04/20 15:48:08 by mle-roy           #+#    #+#             */
+/*   Updated: 2014/04/20 18:14:00 by mle-roy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
 
+#define TINY_M 512
+#define SMALL_M 4096
+
+typedef struct		s_big
+{
+//	int			addr;
+	t_big		*next;
+//	void		*pool;
+}					t_big;
+
+typedef struct		s_sm
+{
+//	int			addr;
+	int			tab[100];
+	t_sm		*next;
+//	void		*pool;
+}					t_sm;
 
 typedef struct		s_malloc
 {
-	int			size;
-	char		free;
+	t_sm		*small_m;
+	t_sm		*medium_m;
+	t_big		*big_m;
 }					t_malloc;
+
+
+	t_malloc			pool;
+
+
+void	init_small(t_sm **mem)
+{
+	int		i;
+
+	i = 0;
+	while (i < 100)
+	{
+		*(mem)->tab[i] = 0;
+		i++;
+	}
+	*(mem)->next = NULL;
+}
+
+void	*find_alloc(t_sm *mem)
+{
+	int		i;
+
+	i = 0;
+	while (i < 100)
+	{
+		if (mem->tab[i] == 0)
+		{
+			return (mem + ((i + 1) * TINY_M));
+		}
+	}
+	return (NULL);
+}
+
+void	*small_malloc(size_t size)
+{
+	void	*ret;
+	t_sm	*bws_small;
+	t_sm	*keep;
+
+	if (!pool.small_m)
+	{
+		pool.small_m = (t_sm *)mmap(0, TINY_M * 101, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE);
+		init_small(&(pool.small_m));
+	}
+	bws_small = pool.small_m;
+	while (bws_small)
+	{
+		if ((ret = find_alloc(bws_small)))
+			return (ret);
+		keep = bws_small;
+		bws_small = bws_small->next;
+	}
+	keep->next = (t_sm *)mmap(0, TINY_M * 101, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE);
+	return (find_alloc(keep->next));
+}
 
 void	*ft_malloc(size_t size)
 {
-	static char		flag_1 = 0;
-	static int		pg_size = 0;
+	static char			flag = 0;
+//	static int			pg_size = 0;
 
 	if (size <= 0)
 		return (NULL);
-	if (!flag_1)
+	if (!flag)
 	{
-		if ((pg_size = getpagesize()) <= 0)
-			return (NULL);
-		flag_1 = 1;
+		pool.small_m = NULL;
+		pool.medium_m = NULL;
+		pool.big_m = NULL;
+		flag = 1;
 	}
-	if (size <= (pg_size / 100))
+	if (size <= TINY_M)
 	{
-		small_malloc(size, pg_size);
+		return (small_malloc(size));
 	}
-	else if (size > (pg_size / 100) && size <= pg_size)
+	else if (size <= SMALL_M)
 	{
-		medium_malloc(size, pg_size);
+		return (medium_malloc(size));
 	}
 	else
 	{
-		big_malloc(size, pg_size);
+		return (big_malloc(size));
 	}
 }
 
